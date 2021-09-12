561d54c3b2adf664a630c995dc3cc1a2ec9b8af6
rii amri <riiamri23@gmail.com>
2021-09-12 20:24:18 +0700

    test

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,17 @@
 <template>
   <Nuxt />
 </template>
-
+<style scoped>
+.bg-image{
+  background-image:url('https://source.unsplash.com/1L71sPT5XKc');
+}
+</style>
 <script>
 export default {
   head() {
     return {
       bodyAttrs: {
-        class: "bg-gray-300",
+        class: "font-sans antialiased text-gray-900 leading-normal tracking-wider bg-cover bg-image",
       },
     };
   },
