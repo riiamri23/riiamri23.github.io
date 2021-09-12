b156cfc38b59339bc5dd9e7ec8c41b186d95b6e6
rii amri <riiamri23@gmail.com>
2021-09-12 16:21:13 +0700

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   target: 'static',
   router:{
-    base: 'https://riiamri23.github.io/'
+    base: '/'
   },
   head: {
     title: 'portfolio',
