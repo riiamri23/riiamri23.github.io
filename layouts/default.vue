a5b57af5257e131bbfcb5785680b94e4d134bf9f
rii amri <riiamri23@gmail.com>
2021-09-12 20:11:31 +0700

    test

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,5 @@
 <template>
-  <div class="flex-row justify-center w-full min-h-screen">
-    <div
-      class="w-8/12 bg-white shadow-md overflow-hidden mx-auto"
-    >
-      <Nav />
-      <Nuxt />
-    </div>
-  </div>
+  <Nuxt />
 </template>
 
 <script>
