94c6d0d248c2144df609ca818dc2e3f3655c54ec
riiamri <syaeful.amri@dataon.com>
2022-09-12 21:24:58 +0700

    update

--- _nuxt/static/1662992369/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002FAbout","\u002FContact","\u002FProject","\u002FArticle","\u002FExperiences","\u002Fproject","\u002Fcontact","\u002Fabout","\u002Fexperiences"]})
