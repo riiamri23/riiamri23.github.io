b2a4557ab74784e1ed15d8c4bb2781e48136536c
riiamri <syaeful.amri@dataon.com>
2022-09-13 07:31:08 +0700

    update

---
+++ components/Nav.vue
@@ -0,0 +1,46 @@
+<template>
+<nav class="navbar px-2">
+    <div class="container flex flex-wrap justify-between items-center mx-auto md:justify-start my-6">
+        <div class="flex-shrink-0 flex items-center justify-center ">
+            <router-link to="/" class="text-gray-900 px-3 py-2 rounded-md font-medium text-lg" aria-current="page">>Rii.</router-link>
+        </div>
+        <div class="flex md:order-2">
+            <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false" onclick="document.querySelector('#mobile-menu').classList.toggle('hidden')">
+                <span class="sr-only">Open main menu</span>
+                <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+            </button>
+        </div>
+        <div id="mobile-menu" class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1">
+            <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <li>
+                    <router-link to="/about" class="text-gray-900 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">About</router-link>
+                </li>
+                <li>
+                    <router-link to="/project" class="text-gray-900 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</router-link>
+                </li>
+                <li>
+                    <router-link to="/experiences" class="text-gray-900 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Experiences</router-link>
+                </li>
+                <li>
+                    <router-link to="/contact" class="text-gray-900 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact Me</router-link>
+                </li>
+            </ul>
+            <!-- <router-link to="/about" class="text-gray-900 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">About</router-link>
+
+            <router-link to="/project" class="text-gray-900 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</router-link>
+
+            <router-link to="/article" class="text-gray-900 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Articles</router-link>
+
+            <router-link to="/contact" class="text-gray-900 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact Me</router-link> -->
+        </div>
+    </div>
+
+</nav>
+</template>
+
+
+
+<script>
+export default {
+};
+</script>
