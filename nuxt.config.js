c3748cab47b54820089b15497be5c1a627f7ac5d
riiamri <syaeful.amri@dataon.com>
2022-09-15 22:20:38 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,9 +14,9 @@ export default {
       {
         hid: "Syaeful Amri riiamri23",
         name: "Syaeful Amri",
-        content: process.env.npm_package_description || ""
+        content: process.env.npm_package_description || "Syaeful Amri portfolio for work, i can do what you need, my domain web is riiamri github io i hope someone can search my web in their chrome or firefox or whatever, Syaeful Amri Profesional Software Developer, I’ve no life and I’m a programmer. I've sacrificed my life to learn programming languages to build apps. I can make your dream come true with my power and knowledge. Experienced Software Engineer with a demonstrated history of working in the retail industry. Skilled in PHP, CodeIgniter, Back-End Web Development, REST API, and HTML. Strong engineering professional with a Bachelor's degree focused in Information Technology from Universitas Muhammadiyah Purwokerto. Explore something about me"
       },
-      {name: "google-site-verification", content:"dfI6n5cwvZ7Tc3JNF562s-gNCrpjvu0sKfqWklUv3-E"}
+      {name: "google-site-verification", content:"google-site-verification=7E6C57FOX7jNrWR0eQ4XpdZiH0vUN_1t-KdjFhcRHNk"}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -40,12 +40,28 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-analytics',
   ],
+  googleAnalytics: {
+    id: process.env.GOOGLE_ANALYTICS_ID, // Use as fallback if no runtime config is provided
+  },
+  publicRuntimeConfig: {
+    googleAnalytics: {
+      id: process.env.GOOGLE_ANALYTICS_ID
+    }
+  },
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     'nuxt-purgecss',
+    '@nuxtjs/robots',
+    '@nuxtjs/sitemap',
   ],
+  sitemap: {
+    hostname: 'https://riiamri23.github.io/',
+    gzip: true,
+    exclude: ['/*']
+ },
 
   purgeCSS: {
     mode: 'postcss',
